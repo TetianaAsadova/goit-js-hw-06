7cdee776a9c9d696ed9d2ecc0de25310e783c33e
TetianaAsadova <114417194+TetianaAsadova@users.noreply.github.com>
2023-03-06 16:13:43 +0200

    sum2

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,13 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (e.currentTarget.email.value === "" || e.currentTarget.password.value === "") {
+        console.error("Please fill in all the fields!");
+    } else {
+        const { email, password } = e.currentTarget.elements;
+        console.log(`Email: ${email.value}, Password: ${password.value}`);
+        e.currentTarget.reset();
+    }
+});
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,19 @@
+const changeColorBtnRef = document.querySelector('.change-color');
+const colorRef = document.querySelector('.color');
+const bodyRef = document.querySelector('body');
+
+changeColorBtnRef.addEventListener('click', newBackgroundColor);
+
+function newBackgroundColor() {
+  const randomColor = getRandomHexColor();
+  bodyRef.style.backgroundColor = randomColor;
+  colorRef.textContent = randomColor;
+}
+  
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
